bd9d5d99a4247d7dc6694d72fa5c3cef555269cd
Danilo Frazão <dsf.eng17@uea.edu.br>
2022-03-12 17:32:35 -0400

    bluetooth-ble

--- esp32-lora-v2/exemplos/bluetooth_ble/bluetooth_ble.ino
+++ esp32-lora-v2/exemplos/bluetooth_ble/bluetooth_ble.ino
@@ -1,3 +1,8 @@
+/* Bibliotecas para o Display OLED*/
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
 #include <BLEDevice.h>
 #include <BLEServer.h>
 #include <BLEUtils.h>
@@ -9,6 +14,13 @@ bool deviceConnected = false;
 #define SERVICE_UUID           "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_UUID_TX "6d68efe5-04b6-4a85-abc4-c2670b7bf7fd"
 
+/* Pinagem para o Display Oled */
+#define OLED_SDA 4
+#define OLED_SCL 15 
+#define OLED_RST 16
+#define SCREEN_WIDTH 128 
+#define SCREEN_HEIGHT 64
+
 class MyServerCallbacks: public BLEServerCallbacks {
   void onConnect(BLEServer* pServer) {
     deviceConnected = true;
@@ -19,8 +31,34 @@ class MyServerCallbacks: public BLEServerCallbacks {
   }
 };
 
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RST); /* Definicões do Display OLED */
+
 void setup() {
   Serial.begin(115200);
+  
+  /* Preparando a inicialização do display OLED */
+  pinMode(OLED_RST, OUTPUT);
+  digitalWrite(OLED_RST, LOW);
+  delay(20);
+  digitalWrite(OLED_RST, HIGH);
+
+  /* Inicialização do display OLED */
+  Wire.begin(OLED_SDA, OLED_SCL);
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3c, false, false)) /* Endereço 0x3C para 128 x 32 */
+  {
+    Serial.println(F("Falha no Display OLED"));
+    for(;;); /* Loop infinito */
+  } 
+  else 
+  {
+    display.clearDisplay();
+    display.setTextSize(1);
+    display.setTextColor(WHITE);
+    display.setCursor(0, 0);
+    display.print("Esperando conexao");
+    display.display();
+    delay(2000);
+  }  
 
   /* Criar o BLE Device */
   BLEDevice::init("ESP32");
@@ -62,7 +100,11 @@ void loop() {
     /* Notificar o cliente conectado */
     pCharacteristic->notify();
 
-    Serial.println("Valor: " + String(txString));
+    Serial.println(txString);
+    display.clearDisplay();
+    display.setCursor(0, 0);
+    display.println(txString);
+    display.display();
 
     cont++;
     delay(1000);
